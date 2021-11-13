bdccbb9e15ea1a339278c6c2bc7e1a6e48fd55b4
Roberto Monteiro <monteiroengenharia@gmail.com>
2021-11-13 13:00:38 -0300

    Update fossaseptica.py

--- fossaseptica.py
+++ fossaseptica.py
@@ -1,13 +1,17 @@
 #Calcula o volume de uma fossa sépica para estabelecimentos residenciais em conformidade com a NBR 7229/1993
 '''
+    Versão Beta
+    Programa desenvolvido por Roberto dos Santos Monteiro
     Entre com os dados da Fossa Séptica
 '''
+#    Apresenta um menu com os tipos de empreendimentos!
+
+print('''\nDIMENSIONAMENTO DE UM TANQUE SÉPTICO
+    Versão Beta 11.2021- desenvolvido por Roberto dos Santos Monteiro.''')
 print('\nVerifique as opões abaixo:\n') # Exibe uma mensagem na tela com o menu de empreendimentos!
 empreendimentos = {1: 'Residencia', 2: 'Hotel', 3: 'Alojamento', 4: 'Escritório', 5: 'Restaurantes e Similares',
           6: 'Fábrica e Geral', 7: 'Edifícios Públicos ou Comerciais', 8: 'Escolas(Externatos) e Locais de Longa Permanência',
           9: 'Bares'}
-    
-    #    Apresenta um menu com os tipos de empreendimentos!
 
 tipo = 1
 while tipo >= 1 or tipo <= 9:
@@ -155,38 +159,52 @@ while N != 0:
             T = 0.50
 
         V = 1000 + N*(C*T + k*Lf)
-        V = V/1000
+        V1 = V/1000
+        print('\nO volume útil da fossa séptica será de %5.2f Litros ou %5.2f m³. Vamos dimensionar a fossa::\n' % (V, V1))
 
-        if V <= 6:
+        if V1 <= 6:
             h = float(input('\nDigite a profunidade entre 1.20m e 2.20m:\n'))
-            A = V/h
-            print('a área útil a ser ocupada pela base da fossa séptica será de %5.2fm².' % A)
-            L = float(input('Defina um valor para um lado da área:\n'))
-            B = A/L
-            print('As dimensões da fossa séptica serão: %5.2fm x %5.2fm x %5.2fm (largura, comprimento, profundidade).' %(L, B, h))
-        elif V > 6.0 or V <= 10:
+            if h < 1.20 or h > 2.20:
+                print('ERROR! O valor digitado está fora do intervalo permitido! O programa será encerrado!')
+                break
+            else:
+                A = V1/h
+                print('a área útil a ser ocupada pela base da fossa séptica será de %5.2fm².' % A)
+                L = float(input('Defina um valor para um lado da área:\n'))
+                B = A/L
+                print('As dimensões da fossa séptica serão: %5.2fm x %5.2fm x %5.2fm (largura, comprimento, profundidade).' %(L, B, h))
+        elif V1 > 6.0 and V1 <= 10:
             h = float(input('Digite a profundidade entre 1.50m e 2.50m:\n'))
-            A = V/h
-            print('a área útil a ser ocupada pela base da fossa séptica será de %5.2fm².' % A)
-            L = float(input('Defina um valor para um lado da área:\n'))
-            B = A/L
-            print('As dimensões da fossa séptica serão: %5.2fm x %5.2fm x %5.2fm (largura, comprimento, profundidade).'%(L, B, h))
-        else:
+            if h < 1.50 or h > 2.50:
+                print('ERROR! O valor digitado está fora do intervalo permitido! O programa será encerrado!')
+                break
+            else: 
+                A = V1/h
+                print('a área útil a ser ocupada pela base da fossa séptica será de %5.2fm².' % A)
+                L = float(input('Defina um valor para um lado da área:\n'))
+                B = A/L
+                print('As dimensões da fossa séptica serão: %5.2fm x %5.2fm x %5.2fm (largura, comprimento, profundidade).'%(L, B, h))
+        elif V1 > 10:
             h = float(input('Digite a profundidade entre 1.80m e 2.80m\n'))
-            A = V/h
-            print('a área útil a ser ocupada pela base da fossa séptica será de %5.2f.' % A)
-            L = float(input('Defina um valor para um lado da área:\n'))
-            B = A/L
-            print('As dimensões da fossa séptica serão: %5.2fm x %5.2fm x %5.2fm (largura, comprimento, profundidade).' %(L, B, h))
+            if h < 1.80 or h > 2.80:
+                print('ERROR! O valor digitado está fora do intervalo permitido! O programa será encerrado!')
+                break
+            else:
+                A = V1/h
+                print('a área útil a ser ocupada pela base da fossa séptica será de %5.2f.' % A)
+                L = float(input('Defina um valor para um lado da área:\n'))
+                B = A/L
+                print('As dimensões da fossa séptica serão: %5.2fm x %5.2fm x %5.2fm (largura, comprimento, profundidade).' %(L, B, h))
                 
-        print('\nMEMÓRIA DE CÁLCULO\n')
-        if tipo == 1:
-            print(f'Polulação: {N} contribuintes;\nPadrão Residencial: {padrao};\nTemperatura Média: {temperatura}C;\nContribuição Diária (C): {C} Litros;\nTemmpo de Detenção (Td): {T};')
-            print(f'Contribuição de Lodo Fresco (Lf): {Lf};\nIntervalo de Limmpeza {limpeza} anos;\nVolume Útil: {V} Litros\n')
-            print('Dimensões: %5.2fm x %5.2fm x %5.2fm (largura, comprimento, profundidade).' %(L, B, h))
-            break
-        else:
-            print(f'Polulação: {N} contribuintes;\nTemperatura Média: {temperatura}C;\nContribuição Diária (C): {C} Litros;\nTemmpo de Detenção (Td): {T};')
-            print(f'Contribuição de Lodo Fresco (Lf): {Lf};\nIntervalo de Limmpeza {limpeza} anos;\nVolume Útil: {V} Litros')
-            print('Dimensões: %5.2fm x %5.2fm x %5.2fm (largura, comprimento, profundidade).\n' %(L, B, h))
-            break
+    print('\nMEMÓRIA DE CÁLCULO\n')
+    if tipo == 1:
+        print(f'Polulação: {N} contribuintes;\nPadrão Residencial: {padrao};\nTemperatura Média: {temperatura}C;\nContribuição Diária (C): {C} Litros;\nTemmpo de Detenção (Td): {T};')
+        print(f'Contribuição de Lodo Fresco (Lf): {Lf};\nIntervalo de Limmpeza {limpeza} anos;\nVolume Útil: {V} Litros;')
+        print('Dimensões: %5.2fm x %5.2fm x %5.2fm (largura, comprimento, profundidade).\n' %(L, B, h))
+        break
+    else:
+        print(f'Polulação: {N} contribuintes;\nTemperatura Média: {temperatura}C;\nContribuição Diária (C): {C} Litros;\nTemmpo de Detenção (Td): {T};')
+        print(f'Contribuição de Lodo Fresco (Lf): {Lf};\nIntervalo de Limmpeza {limpeza} anos;\nVolume Útil: {V} Litros')
+        print('Dimensões: %5.2fm x %5.2fm x %5.2fm (largura, comprimento, profundidade).\n' %(L, B, h))
+        break
+   
